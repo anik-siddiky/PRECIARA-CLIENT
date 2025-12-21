b081fe45c1ed5cef155b55970e830df9c463f3ce
anik-siddiky <codesbynik@gmail.com>
2025-12-21 21:00:35 +0600

    did the bottom part of navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+
+import { IoBagOutline, IoPersonOutline, IoSearchOutline } from 'react-icons/io5';
+import { MdKeyboardArrowDown } from 'react-icons/md';
 import { Link } from 'react-router';
 
 const Navbar = () => {
     return (
         <div className=''>
             <div className=' bg-black text-white text-[11px] font-bold'>
-                <div className='grid grid-cols-3 items-center py-3 px-16'>
+                <div className='grid grid-cols-3 items-center lg:py-3 md:py-2 py-1 px-16'>
                     <div></div>
 
                     <p className='text-center'>30 DAYS RETURN</p>
@@ -19,8 +21,28 @@ const Navbar = () => {
             </div>
 
 
-            <div className='bg-white'>
-                Bottom Nav
+            <div className='bg-white flex px-16 justify-between items-center py-5'>
+                <div className='flex items-center gap-7'>
+                    <h1 className='text-2xl font-extrabold'><Link to='/'>PRECIARA</Link></h1>
+                    <div className='flex text-xs font-bold text-gray-700 space-x-2'>
+                        <div className='flex items-center cursor-pointer'>
+                            <span>RING</span>
+                            <MdKeyboardArrowDown className='mt-0.5' />
+                        </div>
+
+                        <div className='flex items-center cursor-pointer'>
+                            <span>EAR RING</span>
+                            <MdKeyboardArrowDown className='mt-0.5' />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex items-center gap-3 justify-center'>
+                    <p className='font-extralight text-xs text-gray-700 '>Bangladesh</p>
+                    <IoSearchOutline className='mt-0.5' size={30} />
+                    <IoPersonOutline size={28} />
+                    <IoBagOutline size={28} />
+                </div>
             </div>
         </div>
     );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='min-h-screen'>
-            This is Home!!!
+        <div className='min-h-screen bg-gray-300 flex justify-center items-center'>
+            This is Home Page!
         </div>
     );
 };
