d9fa83749ebfb42facdb6181dc4da6b05901fdf8
anik-siddiky <codesbynik@gmail.com>
2025-12-21 08:10:36 +0600

    designed the top part of the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Navbar = () => {
     return (
-        <div>
-            This is Navbar
+        <div className=''>
+            <div className=' bg-black text-white text-[11px] font-bold'>
+                <div className='grid grid-cols-3 items-center py-3 px-16'>
+                    <div></div>
+
+                    <p className='text-center'>30 DAYS RETURN</p>
+
+                    <div className='justify-end space-x-3 hidden md:flex'>
+                        <Link>HELP</Link>
+                        <Link>WISHLIST</Link>
+                        <Link>JOIN US</Link>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='bg-white'>
+                Bottom Nav
+            </div>
         </div>
     );
 };
